bcdcd619b0bfc3cc3ad5915228435f35efe15045
Minod <minod.perera1721@gmail.com>
2023-02-03 10:12:34 +0530

    test file update

--- testfile.js
+++ testfile.js
@@ -1,6 +1,6 @@
 var newList;
 
-newList = ({"List":[
+newList = [
 {"CityCode":"1248991","CityName":"Colombo","Temp":"33.0","Status":"Clouds"},
 {"CityCode":"1850147","CityName":"Tokyo","Temp":"8.6","Status":"Clear"},
 {"CityCode":"2644210","CityName":"Liverpool","Temp":"16.5","Status":"Rain"},
@@ -8,6 +8,10 @@ newList = ({"List":[
 {"CityCode":"2147714","CityName":"Sydney","Temp":"27.3","Status":"Rain"},
 {"CityCode":"4930956","CityName":"Boston","Temp":"4.2","Status":"Mist"},
 {"CityCode":"1796236","CityName":"Shanghai","Temp":"10.1","Status":"Clouds"},
-{"CityCode":"3143244","CityName":"Oslo","Temp":"-3.9","Status":"Clear"}]}).List
+{"CityCode":"3143244","CityName":"Oslo","Temp":"-3.9","Status":"Clear"}]
 
-console.log(newList)
+newList.map(city => {
+    console.log(city.CityName)
+  })
+
+
