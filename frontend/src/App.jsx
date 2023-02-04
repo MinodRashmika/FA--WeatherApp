be76d7f2003d18978c4884c2dcf4a67b49703eb0
Minod <minod.perera1721@gmail.com>
2023-02-04 23:11:36 +0530

    removed import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 
 import {WeatherCard, MultiCardUI} from './pages/index.jsx'
