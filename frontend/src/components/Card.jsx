01f3c7997fa1336e5ec2d473f12730683f2491cb
Minod <minod.perera1721@gmail.com>
2023-02-09 00:07:05 +0530

    added caching data to card component

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,23 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {useCookies} from 'react-cookie'
 
 
 const Card = (props) => {
 
-    //const [cookies, setCookie] = useCookies([]);
-
-    // const handle = () => {
-    //   setCookie(`${cityCode}`, data, { path: '/' });
-    // };
-
-    const[dt, setDt] = useState('');
-    const[Temp, setTemp] = useState('');
-    const[Name, setName] = useState('');
-    const[Desc, setDesc] = useState('');
-    const[Pressure, setPressure] = useState('');
-    const[Humidity, setHumidity] = useState('');
-
+    const date = new Date().getTime()
+    
+    // const[newData, setNewData] = useState();
+    // const[Temp, setTemp] = useState('');
 
     const API_KEY = process.env.REACT_APP_API_KEY
     var url = `https://api.openweathermap.org/data/2.5/weather?id=${props.location}&units=metric&appid=${API_KEY}`
@@ -40,41 +30,73 @@ const Card = (props) => {
       return formattedTime
     }
 
+
     const getData = async () => {
-        axios.get(url).then((res) => {
-            setName(res.data.name)
-            setTemp(res.data.main.temp)
-            setDesc(res.data.weather[0].description)
-            setPressure(res.data.main.pressure)
-            setHumidity(res.data.main.humidity)
-            setDt(res.data.dt)
+  
+      const city = window.localStorage.getItem(`${props.location}`);
+
+      if(city) {
+        if(new Date().getTime() > JSON.parse(city).expDate){
+          window.localStorage.removeItem(`${props.location}`);
+          axios.get(url).then((res) => {
+            localStorage.setItem(`${props.location}`, JSON.stringify({
+              name: res.data.name,
+              temp: res.data.main.temp,
+              desc: res.data.weather[0].description,
+              pressure: res.data.main.pressure,
+              humidity: res.data.main.humidity,
+              dt: res.data.dt,
+              expDate: date + (60000*5) //setting to (60000 * 5) will give 5 mins
+            }))
           })
+          console.log('expired')
+        } else {
+          console.log('all good')
+          axios.get(url).then((res) => {
+            console.log('name = ' + res.data.name + ' & temp = ' + res.data.main.temp + 'ºC updated at ' + setUNIXtime(res.data.dt))
+          })
+        }
+      } else {
+        axios.get(url).then((res) => {
+          localStorage.setItem(`${props.location}`, JSON.stringify({
+            name: res.data.name,
+            temp: res.data.main.temp,
+            desc: res.data.weather[0].description,
+            pressure: res.data.main.pressure,
+            humidity: res.data.main.humidity,
+            dt: res.data.dt,
+            expDate: date + (60000*5) //setting to (60000 * 5) will give 5 mins
+          }))
+        })
+      }
     }
 
+    const cities = JSON.parse(window.localStorage.getItem(`${props.location}`));
 
     useEffect(() => {
-        getData();
-      },[[]])
-
+      // axios.get(url).then((res) => {setTemp(res.da)})
+      getData();    
+    },[])
 
     return(
         <div>
             <div className="p-10">
+              {console.log('expiring time = ' + new Date(cities.expDate))}
             <div class="max-w-lg max-h-lg rounded overflow-hidden shadow-lg">
                 <div className="max-h-[200px] bg-white/20 flex justify-center">
-                    <h1 className="text-black text-8xl flex justify-center">{Temp}</h1>
-                    <h1 className="text-2xl text-black">Cº</h1>
+                    <h1 className="text-black text-8xl flex justify-center">{cities.temp}</h1>
+                    <h1 className="text-2xl text-black">ºC</h1>
                 </div>
             <div class="px-6 py-4  bg-black/80 ">
-                <div class="font-bold text-xl mb-2">{Name}</div>
+                <div class="font-bold text-xl mb-2">{cities.name}</div>
                 <p class="text-white text-base">
-                    {Desc}
+                    {cities.desc}
                 </p>
               </div>
               <div class="px-6 pt-4 pb-2  bg-gray-800/50 ">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Pressure {Pressure}hPa</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Humidity {Humidity}% </span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Last Update {setUNIXtime(dt)}</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Pressure {cities.pressure}hPa</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Humidity {cities.humidity}% </span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Last Update {setUNIXtime(cities.dt)}</span>
               </div>
             </div>
           </div>
